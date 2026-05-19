71acf0c9c2efa45ba32c2bb3b4f47b9615598503
Pablo Liberman <84682262+palmering@users.noreply.github.com>
2026-05-18 22:08:39 -0300

    Refactor service worker for WitraqTool caching
    Updated service worker to use a new cache name and added additional files to cache.

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,24 @@
-self.addEventListener('install', event => {
+// Basic service worker for WitraqTool
+const CACHE_NAME = "witraq-cache-v1";
+const urlsToCache = [
+  "/WitraqTool/",
+  "/WitraqTool/index.html",
+  "/WitraqTool/manifest.json",
+  "/WitraqTool/icons/icon-192.png",
+  "/WitraqTool/icons/icon-512.png"
+];
+
+// Install event: cache core files
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('ble-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch event: serve cached files when offline
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
